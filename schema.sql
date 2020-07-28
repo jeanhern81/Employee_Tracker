20625157e7d927e6124e05656fb1f9e4fd80a44d
jeanhern81 <jeanhern81@gmail.com>
2020-07-28 12:43:56 -0700

    created table in schema file

---
+++ schema.sql
@@ -0,0 +1,27 @@
+DROP DATABASE IF EXIST employee_DB;
+CREATE DATABASE employee_DB;
+
+USE employee_DB;
+
+CREATE TABLE department (
+    id INT NOT NULL AUTO_INCREMENT,
+    name VARCHAR(30) NULL,
+    PRIMARY KEY (id)
+);
+
+
+CREATE TABLE role (
+    id INT NOT NULL AUTO_INCREMENT,
+    title VARCHAR(30) NULL,
+    salary DECIMAL(5,2),
+    departmet_id INT NULL,
+    PRIMARY KEY (id)
+);
+
+CREATE TABLE employee (
+    id INT NOT NULL AUTO_INCREMENT,
+    first_name VARCHAR(30)  NULL,
+    last_name VARCHAR(30) NULL,
+    role_id INT NULL,
+    PRIMARY KEY (id)
+);
